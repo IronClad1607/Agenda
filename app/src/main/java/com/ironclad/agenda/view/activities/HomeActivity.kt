3dae2f5ec2cddc2271ef12102b330e74abb3fd89
Ishaan Jain <ishaan160799@gmail.com>
2020-11-11 12:35:26 +0530

    nav_graph cancel

--- app/src/main/java/com/ironclad/agenda/view/activities/HomeActivity.kt
+++ app/src/main/java/com/ironclad/agenda/view/activities/HomeActivity.kt
@@ -2,8 +2,11 @@ package com.ironclad.agenda.view.activities
 
 import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.ironclad.agenda.R
 import com.ironclad.agenda.databinding.ActivityHomeBinding
 import com.ironclad.agenda.utils.SharedPreferenceHelper
@@ -14,10 +17,24 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
-        val sharedPreferences =
-            getSharedPreferences(SharedPreferenceHelper.spFirstTime, Context.MODE_PRIVATE)
-        val name = sharedPreferences.getString(SharedPreferenceHelper.keyName, "")
-        val wantBiometric = sharedPreferences.getBoolean(SharedPreferenceHelper.keyBiometric, false)
-        binding.tvName.text = "Name: $name Biometric:$wantBiometric"
+
+        binding.fab.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_newTaskFragment)
+        }
+
+        binding.bottomAppBar.setOnMenuItemClickListener { menuItems ->
+            when (menuItems.itemId) {
+                R.id.search -> {
+                    Toast.makeText(this, "Search Tapped", Toast.LENGTH_SHORT).show()
+                    true
+                }
+
+                R.id.accAndSettings -> {
+                    binding.root.findNavController().navigate(R.id.action_homeFragment_to_accountFragment)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
