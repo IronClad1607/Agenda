e4764be899cd08818c5b2715492589be7a4f07d9
Ishaan Jain <ishaan160799@gmail.com>
2020-11-11 15:14:22 +0530

    fragments created and container added to activity_home xml

--- app/src/main/java/com/ironclad/agenda/view/activities/HomeActivity.kt
+++ app/src/main/java/com/ironclad/agenda/view/activities/HomeActivity.kt
@@ -1,12 +1,10 @@
 package com.ironclad.agenda.view.activities
 
-import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.ironclad.agenda.R
 import com.ironclad.agenda.databinding.ActivityHomeBinding
-import com.ironclad.agenda.utils.SharedPreferenceHelper
 
 class HomeActivity : AppCompatActivity() {
 
@@ -14,10 +12,6 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
-        val sharedPreferences =
-            getSharedPreferences(SharedPreferenceHelper.spFirstTime, Context.MODE_PRIVATE)
-        val name = sharedPreferences.getString(SharedPreferenceHelper.keyName, "")
-        val wantBiometric = sharedPreferences.getBoolean(SharedPreferenceHelper.keyBiometric, false)
-//        binding.tvName.text = "Name: $name Biometric:$wantBiometric"
+
     }
 }
